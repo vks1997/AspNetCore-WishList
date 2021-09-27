824ec87668c40a1f2fb0971e7d72ef6139b833ec
vks1997 <86284117+vks1997@users.noreply.github.com>
2021-09-27 14:09:10 +0530

    Initial commit

---
+++ WishList/Controllers/ItemController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WishList.Data;
+using WishList.Models;
+
+namespace WishList.Controllers
+{
+    public class ItemController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ItemController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var model = _context.Items.ToList();
+            return View(model);
+        }
+
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Item item)
+        {
+            _context.Items.Add(item);
+            _context.SaveChanges();
+            return RedirectToAction("~/Item/Index");
+        }
+
+        public IActionResult Delete(int Id)
+        {
+            var item = _context.Items.FirstOrDefault(e=>e.Id==Id);
+            _context.Items.Remove(item);
+            _context.SaveChanges();
+            return RedirectToAction("~/Item/Index");
+        }
+    }
+}
---
+++ WishList/Models/Item.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace WishList.Models
+{
+    public class Item
+    {
+        public int Id ;
+        [Required]
+        [MaxLength(50)]
+        public string Description;
+
+        public static implicit operator DbSet<object>(Item v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
